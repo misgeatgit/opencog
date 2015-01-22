d3dd1cb5ec5cce7f1888711f11ebb18664a916c2
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-01-22 15:22:42 +0800

    enforce underscore naming standard

--- opencog/reasoning/RuleEngine/rule-engine-src/PolicyParams.h
+++ opencog/reasoning/RuleEngine/rule-engine-src/PolicyParams.h
@@ -13,13 +13,13 @@ using namespace std;
 
 //json object key names
 const string RULES = "rules";
-const string FILE_PATH = "file-path";
+const string FILE_PATH = "file_path";
 const string RULE_NAME = "name";
 const string PRIORITY = "priority";
 const string CATEGORY = "category";
-const string ATTENTION_ALLOC = "attention-allocation";
-const string LOG_LEVEL = "log-level";
+const string ATTENTION_ALLOC = "attention_allocation";
+const string LOG_LEVEL = "log_level";
 const string MUTEX_RULES = "mutex";
-const string MAX_ITER = "max-iter";
+const string MAX_ITER = "max_iter";
 
 #endif /* POLICYPARAMS_H_ */
