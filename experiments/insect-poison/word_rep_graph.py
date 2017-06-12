0bfc7494da5e705ef6a1ca9d62637476c1a1e2aa
Misgana Bayetta <misgana.bayetta@gmail.com>
2017-06-12 17:07:05 +0800

    word repetion stat

---
+++ experiments/insect-poison/word_rep_graph.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) != 2:
+  print "Wrong number of params.\n"
+  exit()
+
+fl = sys.argv[1];
+print fl
+word_freq = {}
+word_cycles= {}
+cycle = 0
+
+with open(fl, 'r') as words:
+  read = False
+  for word in words:
+    if word[:6] == "cycle_" :
+      cycle = cycle + 1
+      continue
+    if len(word) != 0:
+      if word in word_cycles:
+        word_cycles[word].append(cycle)
+      else:
+        word_cycles[word] = [cycle]
+
+for word, cycles in word_cycles.iteritems():
+  print word
+  print cycles
+
+word_graph ={}
+for word in word_cycles:
+  for ccl in range(1,cycle+1):
+    bar = ""
+    if ccl in word_cycles[word]:
+      bar += "*"
+    else:
+      bar += " "
+    word_graph[word] = bar
+
+#for word, bar in word_graph.iteritems():
+#  print word+":"+bar
+
+
+
+       
+
