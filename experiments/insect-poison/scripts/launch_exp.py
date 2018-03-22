d512985ea663bea8e5316794088f4862c1a69d6f
Misgana Bayetta <misgana.bayetta@gmail.com>
2018-03-22 18:21:40 +0800

    Update

--- experiments/insect-poison/scripts/launch_exp.py
+++ experiments/insect-poison/scripts/launch_exp.py
@@ -37,35 +37,72 @@ def load_ecan_module() :
   netcat("loadmodule opencog/attention/libattention.so")
 
 def load_experiment_module() :
-  netcat("loadmodule opencog/experiments/insect-poison/libinsect-poison-exp.so")
+  netcat("loadmodule experiments/insect-poison/libinsect-poison-exp.so")
 
 def start_ecan() :
-  nectcat("start-ecan")
+  netcat("start-ecan")
 
 def start_logger() :
-  nectcat("start-logger")
+  netcat("start-logger")
 
-def topic_switched() :
-  nectcat("topic-switched 1")
+def topic_switched(is_on) :
+  netcat("topic-switched "+(is_on==True and '1' or '0'))
 
 def dump_af_stat(zfile) :
-  nectcat("dump-af-stat "+zfile)
+  netcat("dump-af-stat "+zfile)
 
 def parse_sent_file(zfile) :
-  nectcat('(parse-all nlp-parse "'+zfile+'")')
+  netcat('(parse-all nlp-parse "'+zfile+'")')
 
 def parse_sent(sent) :
-  nectcat('(nlp-parse "'+sent+'")')
+  netcat('(nlp-parse "'+sent+'")')
 
 
 def start_word_stimulation(stimulus) :
-  nectcat('(nlp-start-stimulation '+str(stimulus)+')')
+  netcat('(nlp-start-stimulation '+str(stimulus)+')')
 
 
 load_files = ["/home/misgana/Desktop/ECAN/db/conceptnet4.scm",
-    "/home/misgana/Desktop/ECAN/db/wordnet.scm",
-    "/home/misgana/Desktop/ECAN/db/adagram_sm_links.scm"]
+              "/home/misgana/Desktop/ECAN/db/wordnet.scm",
+              "/home/misgana/Desktop/ECAN/db/adagram_sm_links.scm"]
+
+BASE_DIR = "/home/misgana/OPENCOG/opencog"
+
+SENT_DIR = BASE_DIR+"/experiments/insect-poison/data/sentences"
+
+# Atom(uuid), EnteredAt, LastSeenAt, STI, DurationInAF, IsNLPParseOutput, DirectSTI, GainFromSpreading
+def extract_log(column, starting_row, file_name):
+  col = []
+  with open(file_namei, 'r') as log:
+    start = 1
+    for line in log:
+      if(start != starting_row):
+        start = start + 1
+      else:
+        values = line.split(',')
+        col.push(values[column-1])
+
+    return col
 
 if __name__ == "__main__" :
 
+  load_experiment_module()
+  load_ecan_module()
+
   scm_load(load_files)
+  
+  print "Starting ecan and logger agents."
+  start_logger()
+  start_ecan()
+  
+  print "Starting parsing sentences."
+  start_word_stimulation(100)
+  parse_sent_file(SENT_DIR+"/insects.sent")
+
+
+  print "Dumping log data."
+  dump_af_stat("pydump")
+
+
+
+  
