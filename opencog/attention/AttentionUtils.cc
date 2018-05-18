c326751ec97c7da48fdcac1c65a864b6aa868c5f
Misgana Bayetta <misgana.bayetta@gmail.com>
2018-05-18 12:45:46 +0800

    Use classerver::isA method

--- opencog/attention/AttentionUtils.cc
+++ opencog/attention/AttentionUtils.cc
@@ -9,16 +9,8 @@ void removeHebbianLinks(HandleSeq& sources)
         std::remove_if(sources.begin(), sources.end(),
                 [=](const Handle& h)
                 {
-                Type type = h->get_type();
-
-                if (type == ASYMMETRIC_HEBBIAN_LINK ||
-                    type == HEBBIAN_LINK ||
-                    type == SYMMETRIC_HEBBIAN_LINK ||
-                    type == INVERSE_HEBBIAN_LINK ||
-                    type == SYMMETRIC_INVERSE_HEBBIAN_LINK)
-                         return true;
-                else
-                         return false;
+                  Type type = h->get_type();
+                  return classserver().isA(type, HEBBIAN_LINK);
                 });
 
     sources.erase(it_end, sources.end());
